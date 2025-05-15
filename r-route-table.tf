2a112d8ee26f889e8715e8b0c15ea294b2a07019
Mateusz Łoskot <mateusz@loskot.net>
2025-05-15 21:39:09 +0200

    chore: avoid use of deprecated disable_bgp_route_propagation
    Ports fix for #2
    
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- r-route-table.tf
+++ r-route-table.tf
@@ -3,7 +3,7 @@ resource "azurerm_route_table" "route_table" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  disable_bgp_route_propagation = var.disable_bgp_route_propagation
+  bgp_route_propagation_enabled = var.disable_bgp_route_propagation
 
   tags = merge(local.default_tags, var.extra_tags)
 }
